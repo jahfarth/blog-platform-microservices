70da604ea238ff009c5f93b03e79aecfd664e47b
jahfarth <65160558+jahfarth@users.noreply.github.com>
2026-03-23 22:16:05 +0530

    Initialize App component with routing setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import CreatePost from './pages/CreatePost.jsx';
+import PostDetails from './pages/PostDetails.jsx';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/post/:id" element={<PostDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
