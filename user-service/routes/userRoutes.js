8d4006d53e8523d640dd7af841ab1118eabee0df
jahfarth <65160558+jahfarth@users.noreply.github.com>
2026-03-25 10:30:33 +0530

    Add user routes for registration and login

--- user-service/routes/userRoutes.js
+++ user-service/routes/userRoutes.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controller/userController');
 
+router.post('/register', ctrl.register);
+router.post('/login', ctrl.login);
+router.get('/', ctrl.getAllUsers);
+router.get('/:id', ctrl.getProfile);
+
+module.exports = router;
