1fa5ea6165d6a48269046ca894f6b9a43e4c12a4
jahfarth <65160558+jahfarth@users.noreply.github.com>
2026-03-25 10:22:17 +0530

    Add postRoutes with view, react, comments, and reply endpoints

--- blog-service/routes/postRoutes.js
+++ blog-service/routes/postRoutes.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controller/postController');
 
+router.get('/', ctrl.getAllPosts);
+router.get('/:id', ctrl.getPostById);
+router.post('/', ctrl.createPost);
+router.put('/:id', ctrl.updatePost);
+router.delete('/:id', ctrl.deletePost);
+router.post('/:id/view', ctrl.incrementView);
+router.post('/:id/react', ctrl.reactPost);
+router.get('/:id/comments', ctrl.getComments);
+router.post('/:id/comments', ctrl.addComment);
+router.post('/:id/comments/:commentId/reply', ctrl.addReply);
+
+module.exports = router;
